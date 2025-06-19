a002a547b4ae5e71b790e89216c3e04cad2c30fe
ncaraffa <boxtanicolas@gmail.com>
2025-06-19 19:50:16 -0400

    Second commit

---
+++ Curso/src/temperature_converter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class temperature_converter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double temperature;
+        double newTemperature = (temperature)
+
+
+    }
+}
