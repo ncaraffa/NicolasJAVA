d807b66775269d3e7281e46a8bf6675ecee4b3da
Nicolas Caraffa <boxtanicolas@gmail.com>
2025-11-13 16:19:59 -0400

    Second commit

--- Curso/src/overloaded_constructors.java
+++ Curso/src/overloaded_constructors.java
@@ -10,9 +10,28 @@ public class overloaded_constructors {
 
 
         Scanner scanner = new Scanner(System.in);
-        User user = new User("");
+        User user1 = new User("Spongebob");
+        User user2 = new User("Patrick", "patrick@gmai.com");
+        User user3 = new User("Sandy", "sandy@gmail.com", 27);
+        User user4 = new User();
 
-        System.out.println("Digite seu nome de usuário:");
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
+
+        System.out.println(user2.username);
+        System.out.println(user2.email);
+        System.out.println(user2.age);
+
+        System.out.println(user3.username);
+        System.out.println(user3.email);
+        System.out.println(user3.age);
+
+        System.out.println(user4.username);
+        System.out.println(user4.email);
+        System.out.println(user4.age);
+
+        /*System.out.println("Digite seu nome de usuário:");
         user.username = scanner.nextLine();
         while (user.username.isEmpty()) {
             System.out.println("Este campo é obrigatório!");
@@ -28,7 +47,7 @@ public class overloaded_constructors {
         System.out.println("Digite sua idade: ");
         user.age = scanner.nextInt();
 
-        System.out.printf("Seus dados são: %s, %s, %d", user.username, user.email, user.age);
+        System.out.printf("Seus dados são: %s, %s, %d", user.username, user.email, user.age);*/
 
     }
 }
